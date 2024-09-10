0f4e646a4ecc1245885c0ce7b4d0ea12a5d8bf66
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-09-10 11:46:19 +0200

    Added TriviaViewModel

---
+++ app/src/main/java/com/example/whoknows/ui/viewmodels/TriviaViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.whoknows.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class TriviaViewModel : ViewModel() {
+    //TODO: to be implemented
+}
+//Questa classe gestiste la logica dell'applicazione tra Model e View
