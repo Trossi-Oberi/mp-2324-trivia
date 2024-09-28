5c0e82365076eb1882bcb5c1843233ee1d9de27e
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-09-28 14:28:14 +0200

    Updated the whole API logic to handle exceptions and errors correctly using NetworkResponse sealed class and SQLiteExceptions

--- app/src/main/java/it/scvnsc/whoknows/repository/QuestionRepository.kt
+++ app/src/main/java/it/scvnsc/whoknows/repository/QuestionRepository.kt
@@ -1,14 +1,19 @@
 package it.scvnsc.whoknows.repository
 
+import android.database.sqlite.SQLiteException
 import android.util.Log
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import it.scvnsc.whoknows.data.dao.QuestionDAO
 import it.scvnsc.whoknows.data.model.Question
 import it.scvnsc.whoknows.data.network.ApiService
+import it.scvnsc.whoknows.data.network.NetworkResult
 import it.scvnsc.whoknows.data.network.QuestionResponse
 import it.scvnsc.whoknows.data.network.TokenResponse
 import it.scvnsc.whoknows.utils.CategoryManager
 import it.scvnsc.whoknows.utils.QuestionDeserializer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -23,80 +28,132 @@ class QuestionRepository(private val questionDAO: QuestionDAO) {
     }
 
     //Si occupa anche di fare la chiamata API per recuperare le domande
-    val gson = GsonBuilder()
+    private val gson: Gson = GsonBuilder()
         .registerTypeAdapter(Question::class.java, QuestionDeserializer())
         .create()
 
-    val retrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .baseUrl("https://opentdb.com/")
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 
     private val apiService = retrofit.create(ApiService::class.java)
 
-    suspend fun setupInteractionWithAPI() {
-        buildCategoryManager()
-        SESSION_TOKEN = getSessionToken().token
+
+    suspend fun setupInteractionWithAPI(): NetworkResult<Unit> = withContext(Dispatchers.IO) {
+        try {
+            buildCategoryManager()
+            when (val tokenResponse = getSessionToken()) {
+                is NetworkResult.Success -> SESSION_TOKEN = tokenResponse.data.token
+                is NetworkResult.Error -> return@withContext NetworkResult.Error(tokenResponse.exception)
+            }
+            NetworkResult.Success(Unit)
+        } catch (e: Exception) {
+            Log.e("WhoKnows", "Error setting up interaction with API: $e")
+            NetworkResult.Error(e)
+            throw e
+        }
     }
 
     // Step 1: Recuperare le categorie per popolare il CategoryManager (HashMap che collega categoryName e categoryID)
-    private suspend fun buildCategoryManager() {
-        val categories = apiService.getCategories()
-        CategoryManager.buildCategoriesMap(categories.trivia_categories)
+    private suspend fun buildCategoryManager(): NetworkResult<Unit> = withContext(Dispatchers.IO) {
+        try {
+            val categories = apiService.getCategories()
+            CategoryManager.buildCategoriesMap(categories.trivia_categories)
+            Log.d("WhoKnows", "Categories built: ${CategoryManager.categories}")
+            NetworkResult.Success(Unit)
+        } catch (e: Exception) {
+            Log.e("WhoKnows", "Error building categories map: $e")
+            NetworkResult.Error(e)
+            throw e
+        }
     }
 
     // Step 2: Ottieni il Session Token per ottenere risposte sempre diverse dall'API
-    private suspend fun getSessionToken(): TokenResponse {
-        return apiService.getToken()
+    private suspend fun getSessionToken(): NetworkResult<TokenResponse> = withContext(Dispatchers.IO) {
+        try {
+            val response = apiService.getToken()
+            NetworkResult.Success(response)
+        } catch (e: Exception) {
+            NetworkResult.Error(e)
+            throw e
+        }
     }
 
-    suspend fun resetSessionToken() {
-        apiService.resetToken(SESSION_TOKEN)
+    suspend fun resetSessionToken(): NetworkResult<Unit> = withContext(Dispatchers.IO) {
+        try {
+            apiService.resetToken(SESSION_TOKEN)
+
+            Log.d("WhoKnows", "Session token reset: $SESSION_TOKEN")
+
+            NetworkResult.Success(Unit)
+        } catch (e: Exception) {
+            NetworkResult.Error(e)
+            throw e
+        }
     }
 
     //Step 3: Recupera una domanda della categoria e della difficolta' scelte ogni volta che l'utente clicka Play o risponde correttamente a tutte le domande precedenti
-    suspend fun retrieveNewQuestion(categoryName: String, difficulty: String): Question {
+    suspend fun retrieveNewQuestion(categoryName: String, difficulty: String): NetworkResult<Question> = withContext(Dispatchers.IO) {
+        try {
+            //Prendo le nuove domande dall'API
+            var categoryID: String? = ""
+            if (categoryName != "") {
+                categoryID = CategoryManager.categories[categoryName].toString()
+            }
 
-        //Prendo le nuove domande dall'API
-        var categoryID: String? = ""
-        if (categoryName != "") {
-            categoryID = CategoryManager.categories[categoryName].toString()
+            val questionResponse: QuestionResponse = apiService.getQuestions(
+                AMOUNT,
+                categoryID,
+                difficulty,
+                SESSION_TOKEN
+            )
+
+            //Response Code = 4 -> Token Empty, non ci sono altre nuove domande disponibili, resetto il token e rieseguo la query
+            if (questionResponse.response_code == 4) {
+                resetSessionToken()
+                retrieveNewQuestion(categoryName, difficulty)
+            }
+
+            val newFetchedQuestion = questionResponse.results[0]
+            //Log.d("Debug", "Fetched Question: $newFetchedQuestion")
+
+            val newQuestionID: Long = questionDAO.insert(newFetchedQuestion)
+
+            newFetchedQuestion.id = newQuestionID
+            //Log.d("Debug", "New Question ID: ${newFetchedQuestion.id}")
+
+            NetworkResult.Success(newFetchedQuestion)
+        } catch (e: SQLiteException) {
+            NetworkResult.Error(e)
+            throw e
+        } catch (e: Exception) {
+            NetworkResult.Error(e)
+            throw e
         }
-
-        Log.d("Debug", "Category ID: $categoryID")
-        Log.d("Debug", "Difficulty: $difficulty")
-        val questionResponse: QuestionResponse = apiService.getQuestions(
-            AMOUNT,
-            categoryID,
-            difficulty,
-            SESSION_TOKEN
-        )
-        Log.d("Debug", "Question Response: $questionResponse")
-
-        //Response Code = 4 -> Token Empty, non ci sono altre nuove domande disponibili, resetto il token e rieseguo la query
-        if (questionResponse.response_code == 4) {
-            resetSessionToken()
-            retrieveNewQuestion(categoryName, difficulty)
-        }
-
-        val newFetchedQuestion = questionResponse.results[0]
-        Log.d("Debug", "Fetched Question: $newFetchedQuestion")
-        val newQuestionID: Long
-
-        newQuestionID = questionDAO.insert(newFetchedQuestion)
-
-        newFetchedQuestion.id = newQuestionID
-        Log.d("Debug", "New Question ID: ${newFetchedQuestion.id}")
-
-        return newFetchedQuestion
     }
 
-    suspend fun updateLastQuestion(questionID: Long, givenAnswer: String) {
-        return questionDAO.updateLastQuestion(questionID.toInt(), givenAnswer)
+    suspend fun updateLastQuestion(questionID: Long, givenAnswer: String): NetworkResult<Unit> = withContext(Dispatchers.IO) {
+        try {
+            questionDAO.updateLastQuestion(questionID.toInt(), givenAnswer)
+            NetworkResult.Success(Unit)
+        } catch (e: SQLiteException) {
+            Log.e("Database", "Error updating last question in database: $e")
+            NetworkResult.Error(e)
+        }
+        //return questionDAO.updateLastQuestion(questionID.toInt(), givenAnswer)
     }
 
-    suspend fun getQuestionsByIDs(questionsIDs: List<Int>): List<Question> {
-        return questionDAO.getQuestionsByIDs(questionsIDs)
+    suspend fun getQuestionsByIDs(questionsIDs: List<Int>): NetworkResult<List<Question>> = withContext(Dispatchers.IO) {
+        try {
+            val questions = questionDAO.getQuestionsByIDs(questionsIDs)
+            NetworkResult.Success(questions)
+        } catch (e: SQLiteException) {
+            Log.e("Database", "Error retrieving questions by IDs", e)
+            NetworkResult.Error(e)
+        }
+
+        //return questionDAO.getQuestionsByIDs(questionsIDs)
     }
 }
 
