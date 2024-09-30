b5343c0a9c165bea3850fb5f81030ebd2ac70dd4
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-09-30 15:03:45 +0200

    Added PreferencesManager

---
+++ app/src/main/java/it/scvnsc/whoknows/PreferencesManager.kt
@@ -0,0 +1,19 @@
+package it.scvnsc.whoknows
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager(context: Context) {
+    //TODO: da sistemare il suono
+
+
+    private val prefs: SharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+
+    var isDarkTheme: Boolean
+        get() = prefs.getBoolean("isDarkTheme", true)
+        set(value) = prefs.edit().putBoolean("isDarkTheme", value).apply()
+
+    var isSoundEnabled: Boolean
+        get() = prefs.getBoolean("isSoundEnabled", true)
+        set(value) = prefs.edit().putBoolean("isSoundEnabled", value).apply()
+}
